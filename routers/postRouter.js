3b769e0c8d89429989abe4351fd8eda4198bbda5
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-22 22:33:16 +0600

    add router reaction

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -7,4 +7,8 @@ postRouter.post('/', post.createPost);
 
 postRouter.get('/', post.getAllPosts);
 
+postRouter.post('/:id/tweet', post.retweetPost);
+
+postRouter.put('/:id/like', post.updateLike);
+
 exports.postRouter = postRouter;
