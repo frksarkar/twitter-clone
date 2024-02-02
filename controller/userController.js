47d82ae92fb31343233e55de65a74a504c2950da
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-02-02 23:19:16 +0600

    follow action functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,25 @@
-exports.getFollowerAndFollowing = function (req, res, next) {
-    const userId = req.param.userId
+const { User } = require('../module/userSchema');
+
+exports.getFollowerAndFollowing = async function (req, res, next) {
+	const userId = req.params.userId;
 	const { action } = req.query;
-	console.log("🚀 ~ action:", action)
+	const findUser = await User.findById(userId)
+		.select(`${action}`)
+		.populate(`${action}`);
+
 	const Payload = {
 		pageTitle: action,
 		loginUser: req.session.user,
 		loginUserJs: JSON.stringify(req.session.user),
 		activeTab: action,
+		user: findUser,
 	};
 	res.render('followerAndFollowing', Payload);
+};
+
+exports.getFollow = async function (req, res, next) {
+	const userId = req.params.userId;
+	const loginUser = req.session.user._id;
+
+	const user = await User.findByIdAndUpdate(userId, {$addToSet: {followers}})
 };
