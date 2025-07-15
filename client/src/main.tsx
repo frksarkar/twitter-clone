643e00c4c8c3a8b5301d3f55f115432d95b1ca7f
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2025-07-15 21:35:53 +0600

    Add: ThemeProvider to app root for theme context support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './hooks/useTheme';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</StrictMode>
 );
