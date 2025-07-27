7cc5aa9cb0afbc20ce1f03c77194da4faf37516d
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2025-07-27 21:26:06 +0600

    refactor: replace axios with authApi in BookmarksPage component

--- client/src/pages/BookmarksPage.tsx
+++ client/src/pages/BookmarksPage.tsx
@@ -1,34 +1,23 @@
 import { useEffect } from 'react';
 import Tweet from '../components/tweet/Tweet';
 import { MoreHorizontal, Share } from 'lucide-react';
-import axios from 'axios';
-import useAuthStore from '../stores/useAuth';
 import useBookmarkStore from '../stores/useBookmarkStore';
 import useTweetActions from '../hooks/useTweetActions';
 import userStore from '../stores/useAuthUser';
+import { authApi } from '../api';
 
 const BookmarksPage = () => {
-	const { getToken } = useAuthStore();
 	const { getUser } = userStore();
 	const authUser = getUser();
 	const authUserId = authUser?._id || '';
-	const {
-		handleLike: like,
-		handleRetweet: retweet,
-		handleReply,
-		handleBookmark: bookmark,
-		toggleArray,
-	} = useTweetActions();
+	const { handleLike: like, handleRetweet: retweet, handleReply, handleBookmark: bookmark, toggleArray } = useTweetActions();
 	// Filter bookmarked tweets
 	const { setBookmarkTweets, bookmarksTweets } = useBookmarkStore();
 
 	useEffect(() => {
-		axios({
+		authApi({
 			method: 'get',
-			url: 'http://localhost:3000/api/bookmarks',
-			headers: {
-				Authorization: `Bearer ${getToken()}`,
-			},
+			url: '/api/bookmarks',
 		}).then((res) => {
 			setBookmarkTweets(res.data.posts || []);
 		});
@@ -36,9 +25,7 @@ const BookmarksPage = () => {
 
 	const handleLike = (id: string) => {
 		like(id).then(() => {
-			const updatedTweets = bookmarksTweets.map((tweet) =>
-				tweet._id === id ? { ...tweet, likedBy: toggleArray(tweet.likedBy, authUserId) } : tweet
-			);
+			const updatedTweets = bookmarksTweets.map((tweet) => (tweet._id === id ? { ...tweet, likedBy: toggleArray(tweet.likedBy, authUserId) } : tweet));
 
 			setBookmarkTweets(updatedTweets);
 		});
@@ -92,22 +79,13 @@ const BookmarksPage = () => {
 			{bookmarksTweets?.length > 0 ? (
 				<div>
 					{bookmarksTweets.map((tweet) => (
-						<Tweet
-							key={tweet._id}
-							tweet={tweet}
-							onLike={handleLike}
-							onRetweet={handleRetweet}
-							onBookmark={handleBookmark}
-							onReply={handleReply}
-						/>
+						<Tweet key={tweet._id} tweet={tweet} onLike={handleLike} onRetweet={handleRetweet} onBookmark={handleBookmark} onReply={handleReply} />
 					))}
 				</div>
 			) : (
 				<div className="flex flex-col items-center justify-center p-8 text-center h-[50vh]">
 					<h2 className="text-2xl font-bold mb-2">You haven't added any Tweets to your Bookmarks yet</h2>
-					<p className="text-text-secondary-light dark:text-text-secondary-dark mb-6">
-						When you do, they'll show up here.
-					</p>
+					<p className="text-text-secondary-light dark:text-text-secondary-dark mb-6">When you do, they'll show up here.</p>
 				</div>
 			)}
 		</div>
