4a49bc1198c7e473d89b44efc2adfd546dd042fc
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2025-07-27 22:08:03 +0600

    Implement token-based logout with refresh token validation

--- server/controller/logoutController.js
+++ server/controller/logoutController.js
@@ -1,4 +1,19 @@
+const { throwError } = require('../util/helper');
+const { invalidateRefreshToken, verifyToken } = require('../util/tokens');
+
 exports.logout = async function (req, res, next) {
-	req.session.destroy();
-	res.redirect('login');
+	const { refreshToken } = req.cookies;
+
+	try {
+		if (!refreshToken || !verifyToken(refreshToken)) {
+			throwError('invalid refresh token', 403);
+		}
+		res.clearCookie('refreshToken');
+
+		await invalidateRefreshToken(refreshToken);
+
+		res.status(200).json({ status: 'success', message: 'Logout successful' });
+	} catch (error) {
+		return next(error);
+	}
 };
